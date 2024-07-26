fc89516685d9ecc9fc6527850f432a3ed12d2cf2
Aditya bundela <adityabundela1525@gmail.com>
2024-07-26 13:27:44 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,14 +1,3 @@
-function decodeTheRing(string, pattern) {
-  // Convert the pattern into a regex pattern
-  let regexPattern = pattern
-    .replace(/\*/g, '.') // Convert '*' to '.' to match any single character
-    .replace(/\?/g, '.*'); // Convert '?' to '.*' to match zero or more characters
+const decodeTheRing = function (str , pattern)
 
-  // Create a regex object with the pattern
-  let regex = new RegExp('^' + regexPattern + '$'); // Anchors for full match
-
-  // Test the string against the regex
-  return regex.test(string);
-}
-
-module.exports = decodeTheRing;
+if(str.lenght !== pattern.lenght)
